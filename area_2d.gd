9023304f892c83b8ffa97a186c7caf97cf99854e
Amir Calienes <20180317@aloe.ulima.edu.pe>
2024-11-19 17:19:22 -0500

    actualizacion interacciones

--- area_2d.gd
+++ area_2d.gd
@@ -1,10 +1,23 @@
 extends Area2D
 @onready var icono = $interactuar
+@onready var indicacion = get_tree().get_first_node_in_group("interaccion")
+#$/Personajes/Jugador/Camera2D/Control/CanvasLayer/E
+var is_player_near = false  # Señal para saber si el jugador está cerca
 
 func _on_body_entered(body):
-	print("jugador cerca al jefe")
-	icono.show()
+	if body.name == "Jugador":  # Verifica que sea el jugador
+		print("Jugador cerca al jefe")
+		icono.show()
+		indicacion.show()
+		is_player_near = true 
+	
 
 func _on_body_exited(body):
-	print("jugador se aleja del jefe")
-	icono.hide()
+	if body.name == "Jugador":  # Asegúrate de verificar que sea el jugador
+		icono.hide()
+		indicacion.hide()
+		is_player_near = false
+
+func _process(delta):
+	if is_player_near and Input.is_action_just_pressed("interaccion"):
+		Dialogic.start("timeline")
